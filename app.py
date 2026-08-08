756720056d0e825a33e18ccacdb1d8b9ba19281b
asifshaikh <shaikha015@gmail.com>
2026-08-08 12:37:25 +0530

    implemented redis into login and logout

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
-from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity, get_jwt
 from config import Config
 from models import db, User
+from redis_client import redis_client
 import bcrypt
 
 app = Flask(__name__)
@@ -14,6 +15,7 @@ with app.app_context():
 
 @app.route('/')
 def home():
+    
     return {"message": "Redis auth project"}
 
 @app.post('/signup')
@@ -67,12 +69,24 @@ def login():
     access_token = create_access_token(identity=str(user.id))
     refresh_token = create_refresh_token(identity=str(user.id))
 
+    # store access and refresh token in redis
+    redis_client.setex(f"access_token:{user.id}",900,access_token)
+    redis_client.setex(f"refresh_token:{user.id}", 604800, refresh_token)
+
     return jsonify({"access_token": access_token, "refresh_token": refresh_token}), 200
 
 @app.get('/profile')
 @jwt_required()
 def profile():
     user_id = get_jwt_identity()
+
+    access_token = request.headers.get('Authorization').split(" ")[1]
+
+    stored_token = redis_client.get(f"access_token:{user_id}")
+
+    if access_token != stored_token:
+        return jsonify({"error": "Session expired or revoked"}), 401
+    
     user = User.query.get(user_id)
     if not user:
         return jsonify({"error": "User not found"}), 404
@@ -85,6 +99,22 @@ def refresh():
     identity = get_jwt_identity()
     access_token = create_access_token(identity=identity)
     return jsonify({"access_token": access_token}), 200
+
+@app.post('/logout')
+@jwt_required()
+def logout():
+    user_id = get_jwt_identity()
+
+    redis_client.delete(f"access_token:{user_id}")
+    redis_client.delete(f"refresh_token:{user_id}")
+
+    return jsonify({"message": "Logout successful"}), 200
+
+@app.get('/redis-test')
+def redis_test():
+    redis_client.set("test_key", "test_value")
+    value = redis_client.get("test_key")
+    return {"message": value}
 
 if __name__ == '__main__':
     app.run(debug=True)
